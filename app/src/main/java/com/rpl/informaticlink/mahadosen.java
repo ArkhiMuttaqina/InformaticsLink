4afd37b58ffc0df73539bd715cb417f78016c29c
ArkhiMs <arkhi07@hotmail.co.id>
2019-05-28 21:19:44 +0700

    Pindahan dr repo temen suwung

---
+++ app/src/main/java/com/rpl/informaticlink/mahadosen.java
@@ -0,0 +1,16 @@
+package com.rpl.informaticlink;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class mahadosen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mahadosen);
+    }
+}
